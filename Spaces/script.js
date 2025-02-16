da131329e8cac508c5b95a087828e572209918d8
ugadavid <david.subileau@etu.univ-grenoble-alpes.fr>
2025-02-16 18:30:53 +0100

    change SI to Spaces

---
+++ Spaces/script.js
@@ -0,0 +1,10 @@
+import { Game } from './game.js';
+import { KeyboardManager } from './KeyboardManager.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const game = new Game();
+    game.init();
+
+    const keyboardManager = new KeyboardManager(game);
+});
+
