408b900c693efd9400ad83279b63a45c9041ac34
ugadavid <david.subileau@etu.univ-grenoble-alpes.fr>
2025-02-16 18:14:56 +0100

    add docs

---
+++ docs/SI/script.js
@@ -0,0 +1,9 @@
+import { Game } from './game.js';
+import { KeyboardManager } from './KeyboardManager.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const game = new Game();
+    game.init();
+
+    const keyboardManager = new KeyboardManager(game);
+});
